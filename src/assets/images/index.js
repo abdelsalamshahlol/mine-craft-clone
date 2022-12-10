c23658da3b93939327c06797d84826fd12e418c9
Abdelsalam Shahlol <abdulsalamshahlol@gmail.com>
2022-12-10 14:22:04 +0200

    added tailwindcss and texture files, removed fluff

---
+++ src/assets/images/index.js
@@ -0,0 +1,10 @@
+import glass from './glass.jpg';
+import grass from './grass.jpg';
+import log from './log.jpg';
+import wood from './wood.jpg';
+
+const images = {
+    dirst, glass, grass, log, wood
+}
+
+export default images;
